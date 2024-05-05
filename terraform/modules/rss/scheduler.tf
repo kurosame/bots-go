c0f17d69da803df2fd5f5e97faf2dd2928424676
kurosame <kurosame1227@gmail.com>
2024-05-06 00:13:08 +0900

    add cloud scheduler for RSS.app clear cache

--- terraform/modules/rss/scheduler.tf
+++ terraform/modules/rss/scheduler.tf
@@ -1,4 +1,4 @@
-resource "google_cloud_scheduler_job" "this" {
+resource "google_cloud_scheduler_job" "twitter_rss_filter" {
   name     = "twitter-rss-filter"
   schedule = "00 * * * *"
 
@@ -9,6 +9,26 @@ resource "google_cloud_scheduler_job" "this" {
   http_target {
     http_method = "POST"
     uri         = google_cloudfunctions_function.twitter_rss_filter.https_trigger_url
+    headers = {
+      "Content-Type" = "application/json"
+    }
+    oidc_token {
+      service_account_email = google_service_account.this.email
+    }
+  }
+}
+
+resource "google_cloud_scheduler_job" "twitter_rss_filter_clear_cache" {
+  name     = "twitter-rss-filter-clear-cache"
+  schedule = "*/15 * * * *"
+
+  retry_config {
+    retry_count = 3
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloudfunctions_function.twitter_rss_filter_clear_cache.https_trigger_url
     headers = {
       "Content-Type" = "application/json"
     }
